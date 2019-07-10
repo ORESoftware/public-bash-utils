a7d5f4076869dedc3fe3a9a7263a8c882f4125dd
Alexander Mills <alex@alexs-mac.local>
2019-07-10 16:01:26 -0700

    tmp

--- utils.sh
+++ utils.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
 
+zmx_stderr='\033[1;35m'
+zmx_stdout='\033[1;36m'
+zmx_no_color='\033[0m'
+
+
 tailing(){
-  echo "tailing logs for ${name} ...";
+
+  local v="${name-$tag}"  # default is name, tag is backup
+
+  echo "tailing logs for ${v} ...";
+
+  local zmx_var="zmx_$1"
+
   while read line; do
-   echo "$name $1: $line"
+   echo -e "${!zmx_var}$v $1${zmx_no_color}: $line"
   done;
 }
 
 export -f tailing;
+
